f097457074a6dfe149e39d2d6f6ba05642b16606
Ayan Gattani <156289712+I-try-to-code@users.noreply.github.com>
2025-06-09 15:24:40 +0530

    added a ling that threw error by not crating the finance user but tryiing to grant it permission

--- users/create_users.sql
+++ users/create_users.sql
@@ -6,12 +6,14 @@ QUOTA UNLIMITED ON hr_data;
 GRANT CONNECT, RESOURCE TO hr_user;
 
 -- Finance User for accessing Finance Data
+CREATE USER finance_user IDENTIFIED BY financepass
 DEFAULT TABLESPACE finance_data
 TEMPORARY TABLESPACE temp
 QUOTA UNLIMITED ON finance_data;
 GRANT CONNECT, RESOURCE TO finance_user;
 
 -- Sales User for accessing Sales Data
+
 CREATE USER sales_user IDENTIFIED BY salespass
 DEFAULT TABLESPACE sales_data
 TEMPORARY TABLESPACE temp
