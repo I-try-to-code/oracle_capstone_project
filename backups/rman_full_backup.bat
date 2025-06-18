93a0065ebcf070d84806050477ddc2860307c420
Ayan Gattani <156289712+I-try-to-code@users.noreply.github.com>
2025-06-18 12:02:29 +0530

    updated backup scripts and files to take backup correctly and update in logs. additionally, the backup (archivelogs) will be stored in the archivelogs folder

--- backups/rman_full_backup.bat
+++ backups/rman_full_backup.bat
@@ -2,9 +2,6 @@
 echo Starting RMAN Full Backup at %date% %time% > C:\oracle_capstone_project\logs\rman_backup_current.log
 echo. >> C:\oracle_capstone_project\logs\rman_backup_current.log
 
-rman target / << EOF >> C:\oracle_capstone_project\logs\rman_backup_current.log
-BACKUP DATABASE PLUS ARCHIVELOG;
-EXIT;
-EOF
+rman target / @C:\oracle_capstone_project\backups\rman_full_backup_cmd.sql >> C:\oracle_capstone_project\logs\rman_backup_current.log 2>&1
 
 echo Backup completed at %date% %time% >> C:\oracle_capstone_project\logs\rman_backup.log
